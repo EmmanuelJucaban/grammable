4c3bda75f4872cd1be2969e45f5dd0ed41bbeeee
emmanuel <emmanueljucaban@gmail.com>
2016-06-24 08:13:52 +0000

    Added styling to every page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,7 +17,5 @@ class CommentsController < ApplicationController
     params.require(:comment).permit(:message)
   end
 
-  def render_not_found(status=:not_found)
-    render text: "#{status.to_s.titleize} :(", status: status
-  end
+
 end
